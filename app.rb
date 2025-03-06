5cc5dabc56c8a2631477775c82c2c56dded7ab76
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-03-06 23:23:12 +0000

    html done, working on payment

--- app.rb
+++ app.rb
@@ -1,9 +1,55 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_support/all"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+get("/square/new") do
+ 
+  erb(:square_new)
+end
+
+get("/square/results") do
+   @number = params.fetch("number")
+
+  @result = @number.to_i * @number.to_i
+  erb(:square_results)
+end
+
+############################################################################
+get("/square_root/new") do
+
+  erb(:square_root_new)
+end
+get("/square_root/results") do
+  @user_number = params.fetch("user_number").to_i
+  @user_result = Math.sqrt(@user_number)
+  erb(:square_root_result)
+end
+
+
+########################################################################
+
+get("/payment/new") do
+
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  user_apr = params.fetch("user_apr").to_f
+  
+  @apr_result = (user_apr / 100) / 12
+
+  @years = params.fetch("user_years").to_i
+  months = @years * 12
+
+  @principal = params.fetch("user_pv").to_i.to_fs(:currency, { :precision => 2 })
+
+  @payment = (@apr_result * @principal) / (1 - (1+ @apr_result) ** - months).to_fs(:currency, { :precision => 2 })
+
+  erb(:payment_result)
+end
+############################################################################
+get("/random/new") do
+
+  erb(:random_new)
 end
